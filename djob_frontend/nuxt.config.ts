96af74593c77e4f39bd76b8043d6c39d688525f2
Vedant Shukla <118045185+shuklavedant01@users.noreply.github.com>
2025-05-20 10:19:52 +0530

    Update nuxt.config.ts

--- djob_frontend/nuxt.config.ts
+++ djob_frontend/nuxt.config.ts
@@ -1,4 +1,4 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// https://nuxt.com/docs/api/configuration/nuxt-config// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2024-11-01',
@@ -11,4 +11,10 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  runtimeConfig: {
+    public: {
+      apiURL: process.env.NUXT_API_URL || 'http://localhost:8000'
+    }
+  }
 })
+
