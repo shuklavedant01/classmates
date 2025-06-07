62d985d542b357d7530bcc767c39b24f5ea8a828
Vedant Shukla <118045185+shuklavedant01@users.noreply.github.com>
2025-06-07 16:22:43 +0530

    Update login.vue

--- djob_frontend/pages/login.vue
+++ djob_frontend/pages/login.vue
@@ -1,45 +1,41 @@
 <script setup>
-import{useUserStore} from'@/stores/user'
-const router=useRouter()
-const userStore=useUserStore()
+import { useUserStore } from '@/stores/user'
+const config = useRuntimeConfig()
+const router = useRouter()
+const userStore = useUserStore()
 
-let email= ref('')  
-let password=ref('')    
-let errors=ref([]) 
+let email = ref('')
+let password = ref('')
+let errors = ref([])
 
-async function submitForm(){
-    console.log('submitForm')
+async function submitForm() {
+  console.log('submitForm')
+  errors.value = []
 
-    errors.value=[]
-
-    await $fetch('http://127.0.0.1:8000/api/v1/token/login/',{
-        method: 'POST',
-        body:{
-            username: email.value,
-            password: password.value
-        }
-    })
-    .then(data=>{
-        console.log('response', data.auth_token)
-
-        userStore.setToken(data.auth_token, email.value)
-
-        router.push({path: '/'})
-    })
-    .catch(error=>{
-        if (error.response) {
-            for (const property in error.response._data) {
-                errors.value.push(`${property}: ${error.response._data[property]}`)
-            }
-            console.log(JSON.stringify(error.response))
-        } else if (error.message) {
-            errors.value.push('Something went wrong. Please try again')
-            
-            console.log(JSON.stringify(error))
-        }
-    })
+  await $fetch(`${config.public.apiURL}/api/v1/token/login/`, {
+    method: 'POST',
+    body: {
+      username: email.value,
+      password: password.value
+    }
+  })
+  .then(data => {
+    console.log('response', data.auth_token)
+    userStore.setToken(data.auth_token, email.value)
+    router.push({ path: '/' })
+  })
+  .catch(error => {
+    if (error.response) {
+      for (const property in error.response._data) {
+        errors.value.push(`${property}: ${error.response._data[property]}`)
+      }
+      console.log(JSON.stringify(error.response))
+    } else if (error.message) {
+      errors.value.push('Something went wrong. Please try again')
+      console.log(JSON.stringify(error))
+    }
+  })
 }
-
 </script>
 
 
